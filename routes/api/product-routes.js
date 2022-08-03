46f27f8184fbcf5b7732d487f9fc621f03475906
Anisha-C <99618409+Anisha-C@users.noreply.github.com>
2022-08-03 12:44:15 -0400

    created routes and used insomnia to test

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, { model: Tag, through: ProductTag }]
+  }).then((response) => res.json(response))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id,
+    {
+      include: [Category, { model: Tag, through: ProductTag }],
+    }).then((response) => res.json(response))
 });
 
 // create new product
@@ -91,6 +98,9 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: { id: req.params.id }
+  }).then((response) => res.json(response))
 });
 
 module.exports = router;
